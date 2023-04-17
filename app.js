7eb5f589042519f510b4908bd87cdc01dfba05fe
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-04-17 10:38:02 -0400

    Setup routes

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session')
 
+//requiring user route
+const userRoutes = require('./routes/users');
+
 dotenv.config({path : './config.env'});
 
 mongoose.connect(process.env.DATABASE_LOCAL, {
@@ -37,6 +40,9 @@ app.use((req, res, next)=> {
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+app.use(express.static('puublic'));
+
+app.use(userRoutes);
 
 app.listen(process.env.PORT, ()=> {
     console.log('Server has started')
