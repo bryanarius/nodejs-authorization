cb718446e3174b070747d428df74a212e69e4d0f
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-04-18 05:31:58 -0400

    added log in and sign out feature

--- app.js
+++ app.js
@@ -45,13 +45,15 @@ app.use(flash());
 app.use((req, res, next)=> {
     res.locals.success_msg = req.flash(('success_msg'));
     res.locals.error_msg = req.flash(('error_msg'));
+    res.locals.error = req.flash(('error'));
+    res.locals.currentUser = req.user;
     next();
 })
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
-app.use(express.static('puublic'));
+app.use(express.static('public'));
 
 app.use(userRoutes);
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const router = express.Router();
 const passport = require('passport')
+
+
 const User = require('../models/usermodel');
+
+// authentication check
+function isAuthenticatedUser(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next()
+    }
+    req.flash('error_msg', 'Please Login first to access this page.')
+    res.redirect('/login')
+}
+
 //Get routes
 router.get('/login', (req,res)=> {
     res.render('login')
@@ -9,11 +21,23 @@ router.get('/login', (req,res)=> {
 router.get('/signup', (req,res)=> {
     res.render('signup')
 })
-router.get('/dashboard', (req,res)=> {
+router.get('/dashboard', isAuthenticatedUser,(req,res)=> {
     res.render('dashboard')
 })
 
+router.get('/logout', (req, res)=> {
+    req.logOut();
+    req.flash('success_msg', 'You have been logged out.');
+    res.redirect('/login')
+})
+
 //Post routes
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect : '/dashboard',
+    failureRedirect : '/login',
+    failureFlash : 'Invalid email or password'
+}));
 
 router.post('/signup', (req, res)=> {
     let {name, email, password} = req.body;
